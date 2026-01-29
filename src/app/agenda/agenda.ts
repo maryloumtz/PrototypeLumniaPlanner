19584b84487192ca485dd039f6bf190841684585
Edouard <edouard@cineplus.com>
2026-01-29 16:01:12 +0100

    Ajout de la page agenda, pas dev encore ...

---
+++ src/app/agenda/agenda.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './agenda.html',
+  styleUrl: './agenda.css',
+})
+export class Agenda {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { Home } from './home/home';
+import { Agenda } from './agenda/agenda';
 
 export const routes: Routes = [
-  { path: '', component: Home }
+  { path: '', component: Home },
+  { path: 'agenda', component: Agenda },
 ];
